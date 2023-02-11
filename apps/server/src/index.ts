8f1723951a7bf2cf8395c5cf5b46b9608f7f6d6b
Daniel Esteves <31737273+danestves@users.noreply.github.com>
2023-02-11 01:12:42 -0400

    feat: init basic server settings

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,19 @@
-import path from "path"
+import fs from "node:fs"
+import path from "node:path"
 
 import { payload } from "@danestves/cms"
 import { createRequestHandler } from "@danestves/web/express"
+import compression from "compression"
 import express from "express"
 import morgan from "morgan"
+import onFinished from "on-finished"
+
+import { getRedirectsMiddleware } from "./redirects"
+
+const here = (...d: Array<string>) => path.join(__dirname, ...d)
+const primaryHost = "danestves.com"
+const getHost = (req: { get: (key: string) => string | undefined }) =>
+  req.get("X-Forwarded-Host") ?? req.get("host") ?? ""
 
 const MONGODB_URL = process.env.MONGODB_URL ?? ""
 const PAYLOADCMS_SECRET = process.env.PAYLOADCMS_SECRET ?? ""
@@ -15,7 +25,64 @@ const WEB_PUBLIC_BUILD_DIR = path.join(process.cwd(), "../web/public/web/build")
 
 async function start() {
   const app = express()
-  app.disable("x-powered-by")
+
+  app.use(async (req, res, next) => {
+    res.set("X-Powered-By", "danestves LLC")
+    res.set("X-Fly-Region", process.env.FLY_REGION ?? "unknown")
+    res.set("X-Fly-App", process.env.FLY_APP_NAME ?? "unknown")
+    res.set("X-Frame-Options", "SAMEORIGIN")
+
+    const host = getHost(req)
+    if (!host.endsWith(primaryHost)) {
+      res.set("X-Robots-Tag", "noindex")
+    }
+    res.set("Access-Control-Allow-Origin", `https://${host}`)
+
+    // if they connect once with HTTPS, then they'll connect with HTTPS for the next hundred years
+    res.set("Strict-Transport-Security", `max-age=${60 * 60 * 24 * 365 * 100}`)
+    next()
+  })
+
+  app.use(async (req, res, next) => {
+    if (req.get("cf-visitor")) {
+      // console.log(`👺 disallowed cf-visitor`, req.headers) // <-- this can be kinda noisy
+      // make them wait for it... Which should cost them money...
+      await new Promise((resolve) => setTimeout(resolve, 90_000))
+      return res.send("Please go to https://kcd.dev instead! Ping Kent if you think you should not be seeing this...")
+    } else {
+      return next()
+    }
+  })
+
+  app.use((req, res, next) => {
+    const proto = req.get("X-Forwarded-Proto")
+    const host = getHost(req)
+    if (proto === "http") {
+      res.set("X-Forwarded-Proto", "https")
+      res.redirect(`https://${host}${req.originalUrl}`)
+      return
+    }
+    next()
+  })
+
+  app.all(
+    "*",
+    getRedirectsMiddleware({
+      redirectsString: fs.readFileSync(here("./_redirects.txt"), "utf8"),
+    }),
+  )
+
+  app.use((req, res, next) => {
+    if ((req.path.endsWith("/") || req.path.endsWith("admin/")) && req.path.length > 1) {
+      const query = req.url.slice(req.path.length)
+      const safepath = req.path.slice(0, -1).replace(/\/+/g, "/")
+      res.redirect(301, safepath + query)
+    } else {
+      next()
+    }
+  })
+
+  app.use(compression())
 
   // Serving the web static files with different caching strategies
   app.use(
@@ -28,7 +95,31 @@ async function start() {
   )
   app.use("/web", express.static(WEB_PUBLIC_DIR, { maxAge: "1h", redirect: false }))
 
-  app.use(morgan("combined"))
+  // log the referrer for 404s
+  app.use((req, res, next) => {
+    onFinished(res, () => {
+      const referrer = req.get("referer")
+      if (res.statusCode === 404 && referrer) {
+        console.info(`👻 404 on ${req.method} ${req.path} referred by: ${referrer}`)
+      }
+    })
+    next()
+  })
+
+  app.use(
+    morgan((tokens, req, res) => {
+      const host = getHost(req)
+      return [
+        tokens.method?.(req, res),
+        `${host}${tokens.url?.(req, res)}`,
+        tokens.status?.(req, res),
+        tokens.res?.(req, res, "content-length"),
+        "-",
+        tokens["response-time"]?.(req, res),
+        "ms",
+      ].join(" ")
+    }),
+  )
 
   await payload.init({
     express: app,
