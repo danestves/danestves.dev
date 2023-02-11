69b66ff979e552a382e51150d0f81295253c25e8
Daniel Esteves <31737273+danestves@users.noreply.github.com>
2023-02-11 17:34:36 -0400

    fix: put icon inside github

---
+++ apps/cms/src/fields/rich-text/video/icon/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Icon = () => (
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <rect fill="#333333" height="13.63" rx="2.9" width="19.93" x="2.03" y="4.94" />
+    <polygon fill="#ffffff" points="15.34 11.76 10 8.67 10 14.84 15.34 11.76" />
+  </svg>
+)
+
+export default Icon
