991baf3309fd6824194874fb4f00ab33e6fc2770
Daniel Esteves <31737273+danestves@users.noreply.github.com>
2023-03-01 01:50:32 -0400

    feat: adding action for post request on keystatic

--- apps/web/app/routes/api.keystatic.$.ts
+++ apps/web/app/routes/api.keystatic.$.ts
@@ -1,8 +1,12 @@
-import type { LoaderArgs } from "@remix-run/node"
+import type { ActionArgs, LoaderArgs } from "@remix-run/node"
 
 import { handleLoader } from "@keystatic/remix/api"
 
 import config from "keystatic.config"
+
+export async function action(args: ActionArgs) {
+  return handleLoader({ config }, args)
+}
 
 export async function loader(args: LoaderArgs) {
   return handleLoader({ config }, args)
