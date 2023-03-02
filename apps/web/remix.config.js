540a16d2753f4b09514a6971337517dfb92ad031
Daniel Esteves <31737273+danestves@users.noreply.github.com>
2023-03-02 01:25:08 -0400

    feat: dev server with react-refresh from future flag

--- apps/web/remix.config.js
+++ apps/web/remix.config.js
@@ -1,6 +1,11 @@
+require("dotenv").config()
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   future: {
+    unstable_dev: {
+      appServerPort: process.env.PORT,
+    },
     unstable_postcss: true,
     unstable_tailwind: true,
     v2_routeConvention: true,
