9e761fef3d3ff5b22839e0c373cfc3179ac805db
Daniel Esteves <31737273+danestves@users.noreply.github.com>
2023-07-30 21:43:42 -0400

    feat: using process cwd for reader

--- app/utils/reader.server.ts
+++ app/utils/reader.server.ts
@@ -2,4 +2,4 @@ import { createReader } from '@keystatic/core/reader'
 
 import keystaticConfig from 'keystatic.config'
 
-export const reader = createReader('../..', keystaticConfig)
+export const reader = createReader(process.cwd(), keystaticConfig)
